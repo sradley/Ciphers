723a26ac4d387f924efd12c2db66ea98eb068eab
sradley <stephen.t.radley@protonmail.com>
2019-08-20 17:21:07 +1000

    added running key cipher

---
+++ running_key/src/lib.rs
@@ -0,0 +1,196 @@
+//! # Running Key Cipher
+//!
+//! ...
+
+static TABULA_RECTA: [[char; 26]; 26] = [
+    [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ],
+    [
+        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
+    ],
+    [
+        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+    ],
+    [
+        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+        'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
+    ],
+    [
+        'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
+    ],
+    [
+        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
+    ],
+    [
+        'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F',
+    ],
+    [
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+        'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    ],
+    [
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    ],
+    [
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
+        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    ],
+    [
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    ],
+    [
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
+        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+    ],
+    [
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
+        'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    ],
+    [
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
+        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    ],
+    [
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    ],
+    [
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    ],
+    [
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    ],
+    [
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+    ],
+    [
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    ],
+    [
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    ],
+    [
+        'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    ],
+    [
+        'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    ],
+    [
+        'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+    ],
+    [
+        'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+    ],
+    [
+        'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    ],
+    [
+        'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+    ],
+];
+
+#[cfg(test)]
+mod tests {
+    /// `cipher` test function ...
+    #[test]
+    fn cipher() {
+        let plaintext = String::from("DEFENDTHEEASTWALLOFTHECASTLE");
+        let key = String::from("HOWDOESTHEDUCKKNOWTHATSAIDVICTOR");
+
+        let ciphertext = crate::cipher(plaintext, key);
+        assert_eq!(ciphertext, "KSBHBHLALIDMVGKYZKYAHXUAAWGM");
+    }
+
+    /// `decipher` test function ...
+    #[test]
+    fn decipher() {
+        let ciphertext = String::from("KSBHBHLALIDMVGKYZKYAHXUAAWGM");
+        let key = String::from("HOWDOESTHEDUCKKNOWTHATSAIDVICTOR");
+
+        let plaintext = crate::decipher(ciphertext, key);
+        assert_eq!(plaintext, "DEFENDTHEEASTWALLOFTHECASTLE");
+    }
+}
+
+/// `cipher` function ...
+///
+/// ```
+/// let plaintext = String::from("DEFENDTHEEASTWALLOFTHECASTLE");
+/// let key = String::from("HOWDOESTHEDUCKKNOWTHATSAIDVICTOR");
+///
+/// let ciphertext = running_key::cipher(plaintext, key);
+/// assert_eq!(ciphertext, "KSBHBHLALIDMVGKYZKYAHXUAAWGM");
+/// ```
+pub fn cipher(plaintext: String, key: String) -> String {
+    assert!(key.len() >= plaintext.len());
+
+    let key = key.as_bytes();
+
+    let ciphertext = plaintext
+        .bytes()
+        .enumerate()
+        .map(move |(i, c)| {
+            let y = key[i] as usize - 'A' as usize;
+            let x = c as usize - 'A' as usize;
+
+            TABULA_RECTA[y][x] as u8
+        })
+        .collect();
+
+    String::from_utf8(ciphertext).unwrap()
+}
+
+/// `decipher` function ...
+///
+/// ```
+/// let ciphertext = String::from("KSBHBHLALIDMVGKYZKYAHXUAAWGM");
+/// let key = String::from("HOWDOESTHEDUCKKNOWTHATSAIDVICTOR");
+///
+/// let plaintext = running_key::decipher(ciphertext, key);
+/// assert_eq!(plaintext, "DEFENDTHEEASTWALLOFTHECASTLE");
+/// ```
+pub fn decipher(ciphertext: String, key: String) -> String {
+    assert!(key.len() >= ciphertext.len());
+
+    let key = key.as_bytes();
+
+    let plaintext = ciphertext
+        .bytes()
+        .enumerate()
+        .map(move |(i, c)| {
+            let y = key[i] as usize - 'A' as usize;
+
+            let mut x = 0u8;
+            for j in 0..TABULA_RECTA[y].len() {
+                if TABULA_RECTA[y][j] as u8 == c {
+                    x = j as u8;
+                    break;
+                }
+            }
+
+            x + 'A' as u8
+        })
+        .collect();
+
+    String::from_utf8(plaintext).unwrap()
+}
