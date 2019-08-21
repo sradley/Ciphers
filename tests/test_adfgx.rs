dd9ebffa58acadc84f22d0c49bee406584d0f39b
sradley <stephen.t.radley@protonmail.com>
2019-08-21 14:13:02 +1000

    more adfgx tests

--- tests/test_adfgx.rs
+++ tests/test_adfgx.rs
@@ -1,8 +1,8 @@
 use ciphers::adfgx;
 
-/// `cipher` test function ...
+/// `cipher_small` test function ...
 #[test]
-fn cipher() {
+fn cipher_small() {
     let plaintext = String::from("DEFENDTHEEASTWALLOFTHECASTLE");
     let key = String::from("PHQGMEAYNOFDXKRCVSZWBUTIL");
     let keyword = String::from("GERMAN");
@@ -11,13 +11,42 @@ fn cipher() {
     assert_eq!(ciphertext, "FFDGDDADXDAFAFXAAFAFDXDXXFDGDAGDDXXFAFADAFDXDDXDDADGXXGX");
 }
 
-/// `decipher` test function ...
+/// `decipher_small` test function ...
 #[test]
-fn decipher() {
+fn decipher_small() {
     let ciphertext = String::from("FFDGDDADXDAFAFXAAFAFDXDXXFDGDAGDDXXFAFADAFDXDDXDDADGXXGX");
     let key = String::from("PHQGMEAYNOFDXKRCVSZWBUTIL");
     let keyword = String::from("GERMAN");
 
     let plaintext = adfgx::decipher(ciphertext, key, keyword);
     assert_eq!(plaintext, "DEFENDTHEEASTWALLOFTHECASTLE");
+}
+
+/// `cipher_large` test function ...
+#[test]
+fn cipher_large() {
+    let plaintext = String::from("ABCDEFGHIIKLMNOPQRSTUVWXYZABCDEFGHIIKLMNOPQRSTUVWXYZ");
+    let key = String::from("GEHDOSWILFQRCKUXZMNBPATVY");
+    let keyword = String::from("ABCDEFGHIJKLMN");
+
+    let ciphertext = adfgx::cipher(plaintext, key, keyword);
+    assert_eq!(
+        ciphertext,
+        "XAAXFDFGDFXGFFAAGDXDAFFXXFADGGDXFDFGADDGFFAADGADAFFXDGXGGDXXFFADDGAGFDGADAGXDGXXAAXXFFDFXG\
+        AGFGDXDAGXXFAD"
+    );
+}
+
+/// `decipher_large` test function ...
+#[test]
+fn decipher_large() {
+    let ciphertext = String::from(
+        "XAAXFDFGDFXGFFAAGDXDAFFXXFADGGDXFDFGADDGFFAADGADAFFXDGXGGDXXFFADDGAGFDGADAGXDGXXAAXXFFDFXG\
+        AGFGDXDAGXXFAD"
+    );
+    let key = String::from("GEHDOSWILFQRCKUXZMNBPATVY");
+    let keyword = String::from("ABCDEFGHIJKLMN");
+
+    let plaintext = adfgx::decipher(ciphertext, key, keyword);
+    assert_eq!(plaintext, "ABCDEFGHIIKLMNOPQRSTUVWXYZABCDEFGHIIKLMNOPQRSTUVWXYZ");
 }
